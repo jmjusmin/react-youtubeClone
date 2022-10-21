c3b351b05a728f41d235d324e74b56a976b2a22f
jmjusmin <jmjusmin1412@gmail.com>
2022-10-20 21:56:53 -0700

    Complete the basic functions

--- src/component/App.js
+++ src/component/App.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/Youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+
 function App() {
-  const handleTermSubmit = (term) => {
-    youtube.get('/search', {
+  const [videos, setVideo] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const handleTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
       params: {
         q: term,
       },
     });
+
+    setVideo(response.data.items);
+    //set automatic video to appear
+    setSelectedVideo(response.data.items[0]);
   };
 
-  console.log(process.env.REACT_APP_API_KEY);
+  //set a default search when website first render
+  useEffect(() => {
+    handleTermSubmit('buliding');
+  });
+
+  //set a slected video when user click on
+  const onVideoSelect = (video) => {
+    setSelectedVideo(video);
+  };
+
   return (
     <div className="ui container">
       <SearchBar handleTermSubmit={handleTermSubmit} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList videos={videos} onVideoSelect={onVideoSelect} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/component/VideoList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import VideoItem from './VideoItem';
+
+function VideoList({ videos, onVideoSelect }) {
+  const renderList = videos.map((video) => {
+    return (
+      <VideoItem
+        video={video}
+        onVideoSelect={onVideoSelect}
+        key={video.id.videoId}
+      />
+    );
+  });
+
+  return <div className="ui relaxed divided list">{renderList}</div>;
+}
+
+export default VideoList;
